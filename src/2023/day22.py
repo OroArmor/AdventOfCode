dc20dc406bc67a04e14f8fa78be666c533380033
Eli Orona <eliorona@live.com>
2023-12-22 12:34:28 -0800

    AoC 2023 Day 22 Fast

--- src/2023/day22.py
+++ src/2023/day22.py
@@ -33,65 +33,56 @@ TASK_1 = (None, None)
 def task1(input):
     global TASK_1
     cubes = deepcopy(input)
-
     cubes = sorted(cubes, key=lambda c: c[0][2])
 
-    last = set()
-    current = create_filled(cubes)
+    #                             z, cube
+    max_zs = defaultdict(lambda: (0, None))
 
-    while last != current:
-        new_cubes = []
-        for (px, py, pz), (sx, sy, sz) in cubes:
-            if pz - 1 > 0:  # bottom check
-                if sx == sy and (px, py, pz - 1) not in current:  # If the brick is straight up, check if we can drop
-                    new_cubes.append(((px, py, pz - 1), (sx, sy, sz)))
-                elif all([(px + x, py, pz - 1) not in current for x in range(sx)]) and all([(px, py + y, pz - 1) not in current for y in range(sy)]):  # Else check all below and see if cube can go down
-                    new_cubes.append(((px, py, pz - 1), (sx, sy, sz)))
-                else:  # Else keep old cube
-                    new_cubes.append(((px, py, pz), (sx, sy, sz)))
-            else:  # Else keep old cube
-                new_cubes.append(((px, py, pz), (sx, sy, sz)))
+    #                         supported_by, supporting
+    structure = defaultdict(lambda: (set(), set()))
 
-        last = current
-        current = create_filled(new_cubes)
-        cubes = new_cubes
+    for i, ((px, py, pz), (sx, sy, sz)) in enumerate(cubes):
+        z, supported_by = reduce(
+            lambda acc, c: (max(c[0], acc[0]), acc[1] if c[1] is None else ({c[1]} if c[0] > acc[0] else (acc[1].union({c[1]}) if c[0] == acc[0] else acc[1]))),
+            (max_zs[(px + x, py + y)] for x in range(sx) for y in range(sy)),
+            (0, set()))
 
-    supporting = defaultdict(set)
-    for i, cube in enumerate(cubes):
-        this_blocks = create_filled([cube])
-        for j, other in enumerate(cubes):
-            if i == j:
-                continue
-            other_blocks = create_filled([other])
+        for x in range(sx):
+            for y in range(sy):
+                max_zs[(px + x, py + y)] = (z + sz, i)
 
-            for (x, y, z) in other_blocks:
-                if (x, y, z - 1) in this_blocks:
-                    supporting[j].add(i)
+        structure[i] = (supported_by, structure[i][1])
+
+        for j in supported_by:
+            structure[j][1].add(i)
 
     only_supporters = set()
-    for i in supporting:
-        if len(supporting[i]) == 1:
-            only_supporters = only_supporters.union(supporting[i])
+    for i in structure:
+        if len(structure[i][0]) == 1:
+            only_supporters = only_supporters.union(structure[i][0])
 
-    TASK_1 = supporting, only_supporters
+    TASK_1 = structure, only_supporters
 
     return len(cubes) - len(only_supporters)
 
 
 def task2(input):
-    supporting, only_supporters = TASK_1
+    structure, only_supporters = TASK_1
 
     total = 0
     for removed in only_supporters:
         fallen = set()
+        could_fall = structure[removed][1]
         to_fall = {removed}
         while to_fall:
             falling = to_fall.pop()
             fallen.add(falling)
 
-            for cube in supporting:
-                if supporting[cube].issubset(fallen) and cube not in fallen:
+            for cube in could_fall:
+                if structure[cube][0].issubset(fallen) and cube not in fallen:
                     to_fall.add(cube)
+                    could_fall = could_fall.union(structure[cube][1])
+                    could_fall.remove(cube)
 
         total += len(fallen) - 1  # Need to exclude the brick we removed
 
