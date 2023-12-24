363342f59fa11a0658d6e3b50d0d7c1208f98aa8
Eli Orona <eliorona@live.com>
2023-12-23 22:44:41 -0800

    AoC 2023 Day 24 Cleaned Up

--- src/2023/day24.py
+++ src/2023/day24.py
@@ -1,12 +1,6 @@
-import decimal
-
-import scipy
 import sympy as sym
-import sympy.solvers
 
 import util
-from util import *
-import numpy as np
 
 test_data: str = \
     """19, 13, 30 @ -2, 1, -2
@@ -18,7 +12,7 @@ test_data: str = \
 
 def task1(input):
     mi, ma = 200000000000000, 400000000000000
-    if len(input) < 100:
+    if len(input) == 5:
         mi, ma = 7, 27
 
     total = 0
@@ -37,31 +31,27 @@ def task1(input):
             y = bvy / bvx * (x - bx) + by
 
             if mi <= x <= ma and mi <= y <= ma:  # inside box
-                if (x - ax) / avx > 0:  # meet in the future
-                    if (x - bx) / bvx > 0:
-                        if (y - ay) / avy > 0:
-                            if (y - by) / bvy > 0:
-                                total += 1
-                                # assert (x - ax) / avx > 0, (x - ax) / avx
+                if (x - ax) / avx > 0 and (x - bx) / bvx > 0 and (y - ay) / avy > 0 and (y - by) / bvy > 0:  # meet in the future
+                    total += 1
 
     return total
 
 
 def task2(input):
-    # rx + t * rvx == tx + t * tvx
-    # ry + t * rvy == ty + t * tvy
-    # rz + t * rvz == tz + t * tvz
-
     tx, ty, tz, tvx, tvy, tvz = sym.var("tx, ty, tz, tvx, tvy, tvz", integer=True)
     ts = sym.var("t1, t2, t3", positive=True, integer=True)
 
     eqs = []
     for i, ((rx, ry, rz), (rvx, rvy, rvz)) in enumerate(input[:3]):
-        eqs.append(int(rx) + ts[i] * int(rvx) - (tx + ts[i] * tvx))
-        eqs.append(int(ry) + ts[i] * int(rvy) - (ty + ts[i] * tvy))
-        eqs.append(int(rz) + ts[i] * int(rvz) - (tz + ts[i] * tvz))
+        # rx + t * rvx == tx + t * tvx
+        # ry + t * rvy == ty + t * tvy
+        # rz + t * rvz == tz + t * tvz
 
-    res = sympy.solvers.solve(eqs, (*ts, tx, ty, tz, tvx, tvy, tvz))[0]
+        eqs.append(rx + ts[i] * rvx - (tx + ts[i] * tvx))
+        eqs.append(ry + ts[i] * rvy - (ty + ts[i] * tvy))
+        eqs.append(rz + ts[i] * rvz - (tz + ts[i] * tvz))
+
+    res = sym.solvers.solve(eqs, (*ts, tx, ty, tz, tvx, tvy, tvz))[0]
     _, _, _, x, y, z, _, _, _ = res
 
     return x + y + z
@@ -71,7 +61,6 @@ def parse(data: str):
     lines = util.as_lines(data)
 
     stones = []
-
     for line in lines:
         stone, v = line.split(" @ ")
         stone, v = util.as_csv_of_ints(stone), util.as_csv_of_ints(v)
