de8876c13e2f99d1dc605553a0973522e36010b3
Eli Orona <eliorona@live.com>
2024-12-01 21:32:58 -0800

    AoC 2024 Day 2

---
+++ src/2024/day2.py
@@ -0,0 +1,68 @@
+import itertools
+
+import util
+from util import *
+import numpy as np
+
+test_data: str = \
+    """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"""
+
+def is_safe(line: Iterable[int]) -> bool:
+    dir = None
+    for (a, b) in itertools.pairwise(line):
+        if dir is None:
+            dir = a < b
+        if 0 < abs(a - b) < 4 and (dir == (a < b)):
+            continue
+        return False
+    return True
+
+def task1(input):
+    total = 0
+
+    for line in input:
+        if is_safe(line):
+            total += 1
+
+    return total
+
+
+def task2(input):
+    total = 0
+
+    for line in input:
+        if is_safe(line):
+            total += 1
+            continue
+
+        for perm in reversed(list(itertools.combinations(line, len(line) - 1))):
+            if is_safe(perm):
+                total += 1
+                break
+
+
+    return total
+
+
+def parse(data: str):
+    lines = util.as_lines(data)
+    lines = [util.as_ssv_ints(line) for line in lines]
+    return lines
+
+
+def main():
+    data: str = util.get(2, 2024)
+    # data = test_data
+    input = parse(data)
+    print(input)
+    print(task1(input))
+    print(task2(input))
+
+
+if __name__ == "__main__":
+    main()
