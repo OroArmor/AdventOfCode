6f18e5026983eb357ec765274036ba7c120221e0
Eli Orona <eliorona@live.com>
2025-12-07 00:01:53 -0800

    AoC 2025 Day 7 Speedup v2

--- src/2025/day7.py
+++ src/2025/day7.py
@@ -22,59 +22,56 @@ test_data: str = \
 ..............."""
 
 
-def get_splits(point, splitters, found):
-    if point in found.keys():
-        return 0
-
-    _, next_splitter = binary_search(splitters[point.x], point.y)
-    splits = 0
-    if next_splitter < len(splitters[point.x]):
-        splitter = Point(point.x, splitters[point.x][next_splitter])
-        if splitter not in found:
-            splits += get_splits(Point(point.x - 1, splitters[point.x][next_splitter]), splitters, found)
-            splits += get_splits(Point(point.x + 1, splitters[point.x][next_splitter]), splitters, found)
-            splits += 1
-            found[splitter] = splits
-    return splits
-
-
 def task1(input):
     s, splitters = input
-    return get_splits(s, splitters, {})
+    beams = {s.x}
 
+    total = 0
 
-@cache
-def get_total_splits(point, splitters):
-    _, next_splitter = binary_search(splitters[point.x], point.y)
-    splits = 0
-    if next_splitter < len(splitters[point.x]):
-        splits += get_total_splits(Point(point.x - 1, splitters[point.x][next_splitter]), splitters)
-        splits += get_total_splits(Point(point.x + 1, splitters[point.x][next_splitter]), splitters)
-        splits += 1
-    return splits
+    for splitter_line in splitters:
+        splits = splitter_line.intersection(beams)
+        new_beams = splits.symmetric_difference(beams)
+        for split in splits:
+            new_beams.add(split - 1)
+            new_beams.add(split + 1)
+        total += len(splits)
+        beams = new_beams
+
+    return total
 
 
 def task2(input):
-    get_total_splits.cache_clear()
     s, splitters = input
-    return get_total_splits(s, splitters) + 1
+    beams = {s.x: 1}
+
+    for splitter_line in splitters:
+        splits = splitter_line.intersection(set(beams.keys()))
+        new_beams = defaultdict(int)
+        for split in splits:
+            new_beams[split - 1] += beams[split]
+            new_beams[split + 1] += beams[split]
+        for old_beam in set(beams.keys()).difference(splitter_line):
+            new_beams[old_beam] += beams[old_beam]
+        beams = new_beams
+
+    return sum(beams.values())
 
 
 def parse(data: str):
     lines = util.as_lines(data)
 
     s = None
-    splitters = defaultdict(list)
+    splitters: List[set] = []
     for y, line in enumerate(lines):
-        for x, c in enumerate(line):
-            if c == 'S':
-                s = Point(x, y)
-            elif c == '^':
-                splitters[x].append(y)
-    splitters[0] = []
-    splitters[x] = []
+        if y % 2 == 0:
+            splitters.append(set())
+            for x, c in enumerate(line):
+                if c == 'S':
+                    s = Point(x, y)
+                elif c == '^':
+                    splitters[y // 2].add(x)
 
-    return s, HashableDict(splitters)
+    return s, splitters
 
 
 def main():
