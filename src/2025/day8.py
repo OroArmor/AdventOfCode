f405f4173875a88a05532a9b1f4d400231105c80
Eli Orona <eliorona@live.com>
2025-12-08 11:55:08 -0800

    AoC 2025 Day 8 Faster
    50ms -> 20ms

--- src/2025/day8.py
+++ src/2025/day8.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 import util
 from util import *
 import numpy as np
@@ -29,29 +27,11 @@ test_data: str = \
 DISTANCES = None
 
 
-def add_link(i, j, junctions, circuits):
-    if i in junctions.keys() and j in junctions.keys():
-        if junctions[i] == junctions[j]:
-            pass
-        else:
-            to = min(junctions[i], junctions[j])
-            _from = max(junctions[i], junctions[j])
-            for junc in circuits[_from]:
-                junctions[junc] = to
-                circuits[to].add(junc)
-            circuits[_from] = set()
-    elif i in junctions.keys():
-        junctions[j] = junctions[i]
-        circuits[junctions[i]].add(j)
-    elif j in junctions.keys():
-        junctions[i] = junctions[j]
-        circuits[junctions[j]].add(i)
-    else:
-        c = len(circuits)
-        junctions[i] = c
-        junctions[j] = c
-        circuits[c].add(i)
-        circuits[c].add(j)
+def add_link(i, j, junctions):
+    if junctions[i] != junctions[j]:
+        to = min(junctions[i], junctions[j])
+        _from = max(junctions[i], junctions[j])
+        junctions[junctions == _from] = to
 
 
 P1_CHECKPOINT = None
@@ -61,30 +41,32 @@ def task1(input):
     global DISTANCES, P1_CHECKPOINT
 
     points = np.array(input)
-    diff = points[:, None, :] - points[None, :, :]
-    dist_sq = np.sum(diff ** 2, axis=-1)
     I, J = np.triu_indices(len(points), k=1)
-    dist_sq = dist_sq[I, J]
-    DISTANCES = np.argsort(dist_sq)
+    diff = points[I] - points[J]
+    dist_sq = np.einsum('ij,ij->i', diff, diff)
 
-    circuits = defaultdict(set)
-    junctions = {}
+    upper_bound = (10_000 / len(dist_sq) * (dist_sq.max() - dist_sq.min()) + dist_sq.min()) * 2
+    mask = dist_sq < upper_bound
+    DISTANCES = np.argsort(dist_sq[mask])
+    DISTANCES = np.where(mask)[0][DISTANCES]
+
+    junctions = np.arange(len(points))
     for k in range(10 if len(input) == 20 else 1000):
         i, j = I[DISTANCES[k]], J[DISTANCES[k]]
-        add_link(i, j, junctions, circuits)
+        add_link(i, j, junctions)
 
-    P1_CHECKPOINT = (junctions, circuits, I, J)
+    P1_CHECKPOINT = (junctions, I, J)
 
-    return reduce(int.__mul__, list(sorted(map(len, circuits.values())))[-3:], 1)
+    return np.multiply.reduce(np.sort(np.unique_counts(junctions).counts)[-3:])
 
 
 def task2(input):
-    junctions, circuits, I, J = P1_CHECKPOINT
+    junctions, I, J = P1_CHECKPOINT
     for k in itertools.count(10 if len(input) == 20 else 1000):
         i, j = I[DISTANCES[k]], J[DISTANCES[k]]
-        add_link(i, j, junctions, circuits)
+        add_link(i, j, junctions)
 
-        if len(circuits[junctions[j]]) == len(input) or len(circuits[junctions[i]]) == len(input):
+        if np.all(junctions == 0):
             return input[i][0] * input[j][0]
 
 
